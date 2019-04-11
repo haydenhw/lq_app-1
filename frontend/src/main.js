78a3b25ff22c1892597dddd13f023b1bcf439676
hayden7913 <hayden321@gmail.com>
2019-04-10 17:53:26 -0700

    adds light dimming feature

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -13,8 +13,10 @@ if (process.env.NODE_ENV === 'production') {
   console.log('location info:', window.location);
 }
 
+
 Vue.use(
   new VueSocketIO({ connection: 'http://25.81.56.126:8888/data' }),
+  // new VueSocketIO({ connection: 'http//:localhost:8888/data' }),
   // new VueSocketIO({ connection: `${window.location.hostname}:8888/data` }),
 );
 
