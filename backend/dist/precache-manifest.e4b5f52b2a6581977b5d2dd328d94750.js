9db8916ade55970b0e1eb482acd9e2e31cced731
hayden7913 <hayden321@gmail.com>
2019-09-11 00:24:20 -0700

    working build

---
+++ backend/dist/precache-manifest.e4b5f52b2a6581977b5d2dd328d94750.js
@@ -0,0 +1,178 @@
+self.__precacheManifest = [
+  {
+    "revision": "0426fdfbf8f5df993a77",
+    "url": "/vue/css/app.3f81e402.css"
+  },
+  {
+    "revision": "0426fdfbf8f5df993a77",
+    "url": "/vue/js/app.bdc50eed.js"
+  },
+  {
+    "revision": "2efc05588a76872d8d5d",
+    "url": "/vue/css/chunk-4c306375.6e58c4f6.css"
+  },
+  {
+    "revision": "2efc05588a76872d8d5d",
+    "url": "/vue/js/chunk-4c306375.0d4e55ef.js"
+  },
+  {
+    "revision": "c3d7aac99b41ce201907",
+    "url": "/vue/css/chunk-57f8977a.18cd2634.css"
+  },
+  {
+    "revision": "c3d7aac99b41ce201907",
+    "url": "/vue/js/chunk-57f8977a.c5889cfa.js"
+  },
+  {
+    "revision": "8455dd1f87aaad3def33",
+    "url": "/vue/css/chunk-5878c818.becf85cd.css"
+  },
+  {
+    "revision": "8455dd1f87aaad3def33",
+    "url": "/vue/js/chunk-5878c818.f98cd534.js"
+  },
+  {
+    "revision": "9dcd3d80ecda9c018165",
+    "url": "/vue/css/chunk-5f443b6e.f7442519.css"
+  },
+  {
+    "revision": "9dcd3d80ecda9c018165",
+    "url": "/vue/js/chunk-5f443b6e.5068b671.js"
+  },
+  {
+    "revision": "adb6fe9ed537b7b6e296",
+    "url": "/vue/css/chunk-77b3c4ba.10c1110f.css"
+  },
+  {
+    "revision": "adb6fe9ed537b7b6e296",
+    "url": "/vue/js/chunk-77b3c4ba.fe551f18.js"
+  },
+  {
+    "revision": "f3d8f45296992087e13c",
+    "url": "/vue/css/chunk-7958704e.9bd84504.css"
+  },
+  {
+    "revision": "f3d8f45296992087e13c",
+    "url": "/vue/js/chunk-7958704e.0cc98515.js"
+  },
+  {
+    "revision": "02b284a6682c109b10ca",
+    "url": "/vue/css/chunk-b2a0fd02.473b4703.css"
+  },
+  {
+    "revision": "02b284a6682c109b10ca",
+    "url": "/vue/js/chunk-b2a0fd02.4c0295a9.js"
+  },
+  {
+    "revision": "1adbd29e385f958ae83a",
+    "url": "/vue/css/chunk-vendors.4384cebb.css"
+  },
+  {
+    "revision": "1adbd29e385f958ae83a",
+    "url": "/vue/js/chunk-vendors.14622935.js"
+  },
+  {
+    "revision": "fd27aec9cd6f7f619a21f5d5bfe3b5ff",
+    "url": "/vue/img/runs-table.fd27aec9.png"
+  },
+  {
+    "revision": "ccf24e58eb5407f34c1a2e3526587163",
+    "url": "/vue/img/reactor-3d-light.ccf24e58.png"
+  },
+  {
+    "revision": "a6152e897b853389ccd196b06095f93f",
+    "url": "/vue/img/space-blue.a6152e89.jpg"
+  },
+  {
+    "revision": "c043d58e2db97496c31985fc8296be20",
+    "url": "/vue/img/kapsel-logo.c043d58e.png"
+  },
+  {
+    "revision": "160b9cd7d155ec08bd4427dc87afb3ef",
+    "url": "/vue/fonts/roboto-condensed-v17-latin-300.160b9cd7.eot"
+  },
+  {
+    "revision": "4f5f5c645bf1a0cd30d0927f363da4a8",
+    "url": "/vue/fonts/roboto-condensed-v17-latin-300.4f5f5c64.woff2"
+  },
+  {
+    "revision": "b8f4e373a619c50643e7dfa36e9474d6",
+    "url": "/vue/fonts/roboto-condensed-v17-latin-300.b8f4e373.woff"
+  },
+  {
+    "revision": "f87018195ced0dad8d4e3e6562c80516",
+    "url": "/vue/fonts/roboto-condensed-v17-latin-300.f8701819.ttf"
+  },
+  {
+    "revision": "4230f241836bcfeebad3db578e0c944e",
+    "url": "/vue/fonts/roboto-condensed-v17-latin-regular.4230f241.eot"
+  },
+  {
+    "revision": "3ada4464a5d4d208c5e3591076c5138a",
+    "url": "/vue/fonts/roboto-condensed-v17-latin-regular.3ada4464.woff2"
+  },
+  {
+    "revision": "5c7a07a5cc1fd3d8196e6f2a2e0f76ff",
+    "url": "/vue/fonts/roboto-condensed-v17-latin-regular.5c7a07a5.woff"
+  },
+  {
+    "revision": "b47c5c18180aeaa8de60282ab7667d6f",
+    "url": "/vue/fonts/roboto-condensed-v17-latin-regular.b47c5c18.ttf"
+  },
+  {
+    "revision": "b7acb4253d004d9ec348010d487b559c",
+    "url": "/vue/fonts/roboto-condensed-v17-latin-700.b7acb425.eot"
+  },
+  {
+    "revision": "6b222561afc4a71b9888f2ec060b72de",
+    "url": "/vue/fonts/roboto-condensed-v17-latin-700.6b222561.woff2"
+  },
+  {
+    "revision": "2b92236bbde5370469497d38ab63938d",
+    "url": "/vue/fonts/roboto-condensed-v17-latin-700.2b92236b.woff"
+  },
+  {
+    "revision": "8b5d9f3f420d19b1b6aa8b08fcd57474",
+    "url": "/vue/fonts/roboto-condensed-v17-latin-700.8b5d9f3f.ttf"
+  },
+  {
+    "revision": "f3ae85bcdd2230f15b5127256b5c97c0",
+    "url": "/vue/img/roboto-condensed-v17-latin-300.f3ae85bc.svg"
+  },
+  {
+    "revision": "8cb983f9db2dc3cf43e7879513126a5c",
+    "url": "/vue/img/roboto-condensed-v17-latin-regular.8cb983f9.svg"
+  },
+  {
+    "revision": "587ed44908df84877c5a7f668eb1dfa1",
+    "url": "/vue/img/roboto-condensed-v17-latin-700.587ed449.svg"
+  },
+  {
+    "revision": "dddfbd08b8f2223363e4f6a206ae8d19",
+    "url": "/vue/img/icomoon.dddfbd08.svg"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "/vue/fonts/MaterialIcons-Regular.e79bfd88.eot"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "/vue/fonts/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "/vue/fonts/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/vue/fonts/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "revision": "4415676f5bee704d235e3accd1740926",
+    "url": "/vue/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue/robots.txt"
+  }
+];
--- frontend/server.js
+++ frontend/server.js
@@ -4,5 +4,6 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use('/vue/', express.static('dist'));
+//app.use('/', express.static('dist'));
 
 app.listen(PORT, () => console.log(`Vue client is running on port ${PORT}!`));
